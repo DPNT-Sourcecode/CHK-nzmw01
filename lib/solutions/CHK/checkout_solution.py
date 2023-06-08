b7392893570e33482cb87eb64533267932d8b52b
sbx_user1051 <sbx_user1051@169.254.136.177>
2023-06-08 20:14:34 +0000

    Thu Jun 08 20:14:34 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -45,15 +45,17 @@ def checkout(skus):
                 sku_E_count = sku_count[sku] // 2
                 # reduce the multiples of B's by the number of multiples of E's
                 sku_count['B'] -= sku_E_count
-                
-
-                # check if there are multiples of B's
-                if sku_count['B'] >= sku_count[sku] // 2:
-                    sku_count['B'] -= sku_count[sku] // 2
-                    sku_count[sku] = 0
-                else:
-                    sku_count[sku] -= sku_count['B'] * 2
+                if sku_count['B'] < 0:
                     sku_count['B'] = 0
+
+
+                # # check if there are multiples of B's
+                # if sku_count['B'] >= sku_count[sku] // 2:
+                #     sku_count['B'] -= sku_count[sku] // 2
+                #     sku_count[sku] = 0
+                # else:
+                #     sku_count[sku] -= sku_count['B'] * 2
+                #     sku_count['B'] = 0
         elif sku == 'B':
             if sku_count[sku] >= 2:
                 total_price += (sku_count[sku] // 2) * 45 + (sku_count[sku] % 2) * 30
@@ -64,3 +66,4 @@ def checkout(skus):
 
     
     
+
