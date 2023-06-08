22193bbb062245ec98f087b3aba106e6d53b6d06
sbx_user1051 <sbx_user1051@169.254.136.177>
2023-06-08 19:23:27 +0000

    Thu Jun 08 19:23:27 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,6 +3,15 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    skus = skus.upper()
+    if not skus:
+        return 0
+    if not isinstance(skus, str):
+        return -1
+    if not all([sku in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' for sku in skus]):
+        return -1
+    if any([skus.count(sku) > 9 for sku in 'STXYZ']):
+        return -1
     
-    raise NotImplementedError()
+
 
