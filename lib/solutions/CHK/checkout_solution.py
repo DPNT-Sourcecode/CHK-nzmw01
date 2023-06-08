de595231d2d33cf89100d953279d37d6433ecd0b
sbx_user1051 <sbx_user1051@169.254.136.177>
2023-06-08 19:29:27 +0000

    Thu Jun 08 19:29:27 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,4 @@
-
+from collections import Counter
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -8,10 +8,12 @@ def checkout(skus):
         return 0
     if not isinstance(skus, str):
         return -1
-    if not all([sku in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' for sku in skus]):
+    # if not all([sku in 'ABCD' for sku in skus]):
+    #     return -1
+    # count frequency of each sku
+    sku_count = Counter(skus)
+    # check if any sku is invalid
+    if any([sku not in 'ABCD' for sku in sku_count.keys()]):
         return -1
-    if any([skus.count(sku) > 9 for sku in 'STXYZ']):
-        return -1
-    if any([skus.count(sku) > 1 for sku in 'STXYZ']):
-        return -1
-    if 
+    
+
